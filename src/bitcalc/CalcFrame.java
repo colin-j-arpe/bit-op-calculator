2e09d9ebe4af6f48e8eb4d532942a3b5197d7547
C. Arpe <colin.j.arpe@outlook.com>
2017-06-19 13:18:39 -0400

    Move button loop to branch

--- src/bitcalc/CalcFrame.java
+++ src/bitcalc/CalcFrame.java
@@ -53,44 +53,34 @@ class DisplayPanel extends JPanel    {
 }
 
 class NumberPanel extends JPanel    {
-    JButton[] numbers = new JButton[11];
-    for (int i = 0; i < 10; i++)    {
-        String buttonLabel = Integer.toString(i);
-        numbers[i].setName(buttonLabel);
-    }
-    numbers[10].setName(".");
-//    JButton b1 = new JButton("1");
-//    JButton b2 = new JButton("2");
-//    JButton b3 = new JButton("3");
-//    JButton b4 = new JButton("4");
-//    JButton b5 = new JButton("5");
-//    JButton b6 = new JButton("6");
-//    JButton b7 = new JButton("7");
-//    JButton b8 = new JButton("8");
-//    JButton b9 = new JButton("9");
-//    JButton b0 = new JButton("0");
-//    JButton point = new JButton(".");
+    JButton b1 = new JButton("1");
+    JButton b2 = new JButton("2");
+    JButton b3 = new JButton("3");
+    JButton b4 = new JButton("4");
+    JButton b5 = new JButton("5");
+    JButton b6 = new JButton("6");
+    JButton b7 = new JButton("7");
+    JButton b8 = new JButton("8");
+    JButton b9 = new JButton("9");
+    JButton b0 = new JButton("0");
+    JButton point = new JButton(".");
     
     public NumberPanel()    {
         super();
         GridLayout numberLayout = new GridLayout(4, 3);
         setLayout(numberLayout);
-        for (int i = 1; i < 10; i++)
-            add(numbers[i]);
-        add(numbers[0]);
-        add(numbers[10]);
         
-//        add(b1);
-//        add(b2);
-//        add(b3);
-//        add(b4);
-//        add(b5);
-//        add(b6);
-//        add(b7);
-//        add(b8);
-//        add(b9);
-//        add(b0);
-//        add(point);
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        add(b5);
+        add(b6);
+        add(b7);
+        add(b8);
+        add(b9);
+        add(b0);
+        add(point);
     }
 }
 
