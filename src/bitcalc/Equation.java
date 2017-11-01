b1d23b9052bb1fe155e2202b0ca1a21215d6f4c3
C. Arpe <colin.j.arpe@outlook.com>
2017-11-01 12:30:54 -0400

    Create addition and subtraction methods, retrofit equation class for inheritance

--- src/bitcalc/Equation.java
+++ src/bitcalc/Equation.java
@@ -9,31 +9,12 @@ public class Equation   {
     final byte RESULT = 2;
     final byte TEMP = 3;
     
-    public boolean addition()    {
-        boolean carryFlag = false;
-        
-        for (int i = 0; i < 32; i++)    {
-            binaryNumber[RESULT][i] = (binaryNumber[OPERAND1][i] ^ binaryNumber[OPERAND2][i]);
-            binaryNumber[RESULT][i] = (binaryNumber[RESULT][i] ^ carryFlag);
-            if (carryFlag)
-                carryFlag = (binaryNumber[OPERAND1][i] || binaryNumber[OPERAND2][i]);
-            else
-                carryFlag = (binaryNumber[OPERAND1][i] && binaryNumber[OPERAND2][i]);
-        }
-
-        if (binaryNumber[OPERAND1][31] == binaryNumber[OPERAND2][31])   {
-            if (binaryNumber[OPERAND1][31]) {
-                return !binaryNumber[RESULT][31];
-            }   else    {
-                return binaryNumber[RESULT][31];
-            }
-        }   else    {
-            return false;
-        }
+    public boolean addition()   {
+        return addRange(0, 32);
     }
-    
+
     public boolean subtraction()   {
-        multByNegOne(OPERAND2);
+        multByNegOne(OPERAND2, 0, 32);
         return addition();
     }
     
@@ -79,46 +60,70 @@ public class Equation   {
             multByNegOne(RESULT);
     }
     
-    private void multByNegOne(byte whichBinary)  {
-        if (binaryNumber[whichBinary][31]) {
-            subtractOne(whichBinary);
-            complement(whichBinary);
+    protected boolean addRange(int startBit, int rangeLength)    {
+        boolean carryFlag = false;
+        int endBit = startBit + rangeLength - 1;
+        
+        for (int i = startBit; i < endBit; i++)    {
+            binaryNumber[RESULT][i] = (binaryNumber[OPERAND1][i] ^ binaryNumber[OPERAND2][i]);
+            binaryNumber[RESULT][i] = (binaryNumber[RESULT][i] ^ carryFlag);
+            if (carryFlag)
+                carryFlag = (binaryNumber[OPERAND1][i] || binaryNumber[OPERAND2][i]);
+            else
+                carryFlag = (binaryNumber[OPERAND1][i] && binaryNumber[OPERAND2][i]);
+        }
+
+        if (binaryNumber[OPERAND1][endBit] == binaryNumber[OPERAND2][endBit])   {
+            if (binaryNumber[OPERAND1][endBit]) {
+                return !binaryNumber[RESULT][endBit];
+            }   else    {
+                return binaryNumber[RESULT][endBit];
+            }
+        }   else    {
+            return false;
+        }
+    }
+    
+    protected void multByNegOne(byte whichNumber, byte start, byte length)  {
+        if (binaryNumber[whichNumber][length - 1]) {
+            subtractOne(whichNumber, start, length);
+            complement(whichNumber, start, length);
         }
         else    {
-            complement(whichBinary);
-            addOne(whichBinary);
+            complement(whichNumber, start, length);
+            addOne(whichNumber, start);
         }
     }
 
-    private boolean subtractOne(byte whichBinary)   {
-        int i = 0;
-        while (!binaryNumber[whichBinary][i] && i < 31)  {
-            binaryNumber[whichBinary][i] = true;
-            i++;
+    protected boolean subtractOne(byte whichNumber, byte start, byte length)   {
+//        int i = start;
+        while (!binaryNumber[whichNumber][start] && start < length - 1)  {
+            binaryNumber[whichNumber][start] = true;
+            start++;
         }
-        if (i == 31)
+        if (start == length - 1)
             return false;
         
-        binaryNumber[whichBinary][i] = false;
+        binaryNumber[whichNumber][start] = false;
         return true;
     }
 
-    private void addOne(byte whichBinary)    {
-        int i = 0;
-        while (binaryNumber[whichBinary][i])   {
-            binaryNumber[whichBinary][i] = false;
+    protected void addOne(byte whichNumber, byte start)    {
+        byte i = start;
+        while (binaryNumber[whichNumber][i])   {
+            binaryNumber[whichNumber][i] = false;
             i++;
         }
-        binaryNumber[whichBinary][i] = true;
+        binaryNumber[whichNumber][i] = true;
     }
 
-    private void complement(byte whichBinary)    {
-        for (int i = 0; i < 32; i++)    {
-            binaryNumber[whichBinary][i] = !binaryNumber[whichBinary][i];
+    protected void complement(byte whichNumber, byte start, byte length)    {
+        for (int i = start; i < length; i++)    {
+            binaryNumber[whichNumber][i] = !binaryNumber[whichNumber][i];
         }
     }
 
-    private boolean removeNegatives()    {
+    protected boolean removeNegatives()    {
         boolean result = binaryNumber[OPERAND1][31] ^ binaryNumber[OPERAND2][31];
         for (byte i = 0; i < 2; i++) {
             if (binaryNumber[i][31])
@@ -127,13 +132,13 @@ public class Equation   {
         return result;
     }
     
-    private boolean bitShiftDouble(byte whichBinary)    {
-        if (binaryNumber[whichBinary][31])
+    protected boolean bitShiftDouble(byte whichNumber)    {
+        if (binaryNumber[whichNumber][31])
             return false;
         for (int i = 31; i > 0; i--)    {
-            binaryNumber[whichBinary][i] = binaryNumber[whichBinary][i-1];
+            binaryNumber[whichNumber][i] = binaryNumber[whichNumber][i-1];
         }
-        binaryNumber[whichBinary][0] = false;
+        binaryNumber[whichNumber][0] = false;
         return true;
     }
 }
---
+++ src/bitcalc/FloatEquation.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitcalc;
+
+/**
+ *
+ * @author cspanw74
+ */
+public class FloatEquation extends Equation {
+    final byte NUMBER_SIZE = 32;
+    final byte MANT_LENGTH = 25;
+    final byte SIGN_BIT = MANT_LENGTH;
+    final byte EXP_START = MANT_LENGTH + 1;
+    final byte EXP_LENGTH = NUMBER_SIZE - EXP_START;
+    
+    @Override
+    public boolean addition()   {
+        byte lesserExponent = checkLesser();
+        while (lesserExponent < 2)   {
+            addOne(lesserExponent, EXP_START);
+            bitShiftHalf(lesserExponent, 0, MANT_LENGTH);
+            lesserExponent = checkLesser();
+        }
+
+        System.arraycopy(binaryNumber[OPERAND1], EXP_START, binaryNumber[RESULT], EXP_START, EXP_LENGTH);
+//        for (byte i = EXP_START; i < EXP_START + EXP_LENGTH; i++)
+//            binaryNumber[RESULT][i] = binaryNumber[OPERAND1][i];
+
+        return addRange(0, MANT_LENGTH + 1);
+    }
+    
+    @Override
+    public boolean subtraction()    {
+        multByNegOne(OPERAND2, 0, MANT_LENGTH + 1);
+        return addition();
+    }
+    
+    private byte checkLesser()  {
+        byte signBit = EXP_START + EXP_LENGTH - 1;
+        if (binaryNumber[OPERAND1][signBit] != binaryNumber[OPERAND2][signBit])
+                return (binaryNumber[OPERAND1][signBit] ? OPERAND1 : OPERAND2);
+        for (byte i = signBit - 1; i >= EXP_START; i--)
+            if (binaryNumber[OPERAND1][i] != binaryNumber[OPERAND2][i])
+                return (binaryNumber[OPERAND1][i] ? OPERAND2 : OPERAND1);
+        return 2;
+    }
+    
+    private void bitShiftHalf(byte whichNumber, byte start, byte length)    {
+        for (byte i = start; i < start + length - 1; i++)
+            binaryNumber[whichNumber][i] = binaryNumber[whichNumber][i+1];
+        binaryNumber[whichNumber][start + length - 1] = false;
+    }
+}
